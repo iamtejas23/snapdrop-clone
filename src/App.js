4a71edf686ef8737a00eeb8bbf8d46c3bfbb73a9
Tejas Mane <tsmane8787@gmail.com>
2024-05-10 01:02:37 +0530

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sharing from './components/Sharing/Sharing';
+// import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div>
       <Sharing/>
+      {/* <Footer/> */}
     </div>
   )
 }
